15474d67b47cb49b94cca61dea09d2ef213f2482
felixjolous47 <yunchez3@uci.edu>
2020-10-11 10:10:23 -0700

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+//prod.js - production keys here
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CILENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookiKey: process.env.COOKIE_KEY
+};
